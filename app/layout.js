0be15a83c9877c00fa9c93e3b9624e63b14260e9
Hyunji-create <hyunji@editionroasters.com>
2026-01-25 18:23:20 +1100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Bar Stock Manager",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>Bar Stock Manager</title>
-        {/* This script makes Tailwind work instantly without complex setup */}
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body className="bg-gray-50 m-0 p-0">{children}</body>
+      <body className="bg-gray-50 m-0 p-0">
+        {children}
+      </body>
     </html>
   );
 }
